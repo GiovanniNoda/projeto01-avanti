f4f96dacf98f5c713122f8b7053480342841f17a
Giovanni <giovanniminoru@gmail.com>
2025-04-18 02:42:22 -0300

    swiper finished

---
+++ scripts/swiper-script.js
@@ -0,0 +1,27 @@
+new Swiper('.swiper', {
+   loop: false,
+   spaceBetween: 8,
+
+   pagination: {
+       el: '.swiper-pagination',
+       clickable: true,
+       dynamicBullets: true
+   },
+
+   navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+   },
+
+   breakpoints: {
+       0: {
+           slidesPerView: 2,
+       },
+       768: {
+           slidesPerView: 3,
+       },
+       1024: {
+           slidesPerView: 5,
+       }
+    }
+})
