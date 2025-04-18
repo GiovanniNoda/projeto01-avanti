34e7134530ef1c12160a6c1e500cef7a9d5aa066
Giovanni <giovanniminoru@gmail.com>
2025-04-18 03:05:06 -0300

    search

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,17 @@ const menu = document.querySelector('.mobile-menu')
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
+
+const searchForm = document.getElementById('search-form')
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const searchTerm = searchForm.querySelector('input').value
+    const searchResult = document.querySelector('.search-result')
+
+    if (!searchTerm) {
+        searchResult.textContent = ''
+        return
+    }
+    searchResult.textContent = `Você procurou por "${searchTerm}"`
+})
