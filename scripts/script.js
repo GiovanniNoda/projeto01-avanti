47724983829ad64b611153f0b9b8dafbe89f1c65
Giovanni <giovanniminoru@gmail.com>
2025-04-21 22:33:51 -0300

    form

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 const toggle = document.querySelector('.menu-toggle')
 const menu = document.querySelector('.mobile-menu')
+const searchForm = document.getElementById('search-form')
+const registerForm = document.getElementById('newsletter-form')
+const nameInput = document.getElementById('name-input')
+const emailInput = document.getElementById('email-input')
+const agreeCheckbox = document.getElementById('agree-checkbox')
 
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
 
-const searchForm = document.getElementById('search-form')
 
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -17,4 +21,13 @@ searchForm.addEventListener('submit', (event) => {
         return
     }
     searchResult.textContent = `Você procurou por "${searchTerm}"`
+})
+
+registerForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    alert('Cadastro feito com sucesso!')
+
+    nameInput.value = ''
+    emailInput.value = ''
+    agreeCheckbox.checked = false
 })
