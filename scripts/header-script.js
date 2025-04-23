4ef397fae893a8d2c1cc0af2e476a1a66151819b
Giovanni <giovanniminoru@gmail.com>
2025-04-22 23:58:38 -0300

    desktop header

--- scripts/header-script.js
+++ scripts/header-script.js
@@ -3,7 +3,7 @@ function renderHeaderLayout() {
 
     headerContainer.innerHTML = ''
 
-    if (window.innerWidth < 768) {
+    if (window.innerWidth < 1024) {
         headerContainer.innerHTML = `
             <div class="header-content">
                 <button class="menu-toggle">
@@ -29,7 +29,65 @@ function renderHeaderLayout() {
         `
     } else {
         headerContainer.innerHTML = `
-            <p> Ola mundo </p>
+            <div class="header-content">
+                <img src="assets/logo.png" alt="Logo Avanti">
+
+                <form id="search-form">
+                    <input type="text" placeholder="Digite aqui o que você procura">
+    
+                    <button><i class="ph ph-magnifying-glass"></i></button>
+                </form>
+
+                <div class="user-info">
+                    <i class="ph ph-user"></i>
+
+                    <div class="user-name">
+                        <span>Olá,</span>
+                        <span>Giovanni Noda!</span>
+                    </div>
+                </div>
+
+                <button class="cart-button">
+                    <i class="ph ph-shopping-cart"></i>
+
+                    <span>2</span>
+                </button>
+            </div>
+
+            <p class="search-result"></p>
+
+            <ul class="menu-desktop">
+                <li>
+                    <button class="menu-toggle">
+                        <i class="ph ph-list"></i>
+                        <span>Todas as Categorias</span>
+                    </button>
+                </li>
+
+                <li>
+                    <a href="#">Departamento</a>
+                </li>
+
+                <li>
+                    <a href="#">Departamento</a>
+                </li>
+
+                <li>
+                    <a href="#">Departamento</a>
+                </li>
+
+                <li>
+                    <a href="#">Departamento</a>
+                </li>
+
+                <li>
+                    <a href="#">Departamento</a>
+                </li>
+                
+                <li>
+                    <a href="#">Departamento</a>
+                </li>
+            </ul>
         `
     }
 } 
